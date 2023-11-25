1ddd4c89a4be8c7ff2ed5de429fa52d69dbd7226
santihoyos15 <santiagohoyos2808@gmail.com>
2023-11-25 00:26:14 -0500

    Finish clase-7

--- src/main/java/ces2/servlets/Servlet1.java
+++ src/main/java/ces2/servlets/Servlet1.java
@@ -1,9 +1,12 @@
 package ces2.servlets;
 
+import ces2.entidades.GetDatos;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.*;
+import java.lang.reflect.Method;
 
 @WebServlet(name = "Servlet1", value = "/Servlet1")
 public class Servlet1 extends HttpServlet {
@@ -17,6 +20,39 @@ public class Servlet1 extends HttpServlet {
 
             int salario = 3;
              datos = calcularBonus(salario);
+
+             String archivo = "sin datos";
+
+             try {
+                 Class<?> clase = Class.forName("ces2.servlets.Servlet1");
+
+                 Method[] methods = clase.getMethods();
+
+                 for (Method method : methods) {
+                     if (method.isAnnotationPresent(GetDatos.class)) {
+                         GetDatos getDatos = method.getAnnotation(GetDatos.class);
+                         archivo = getDatos.archivo();
+                     }
+                 }
+             } catch (ClassNotFoundException e) {
+                 datos = e.toString();
+             }
+
+             if (!archivo.equals("sin datos")) {
+                 try (
+                         FileInputStream myFile = new FileInputStream(archivo);
+                         InputStreamReader inputStreamReader = new InputStreamReader(myFile, "UTF-8");
+                         BufferedReader reader = new BufferedReader(inputStreamReader);
+                 ){
+                     String lectura = "";
+
+                     while ((lectura = reader.readLine()) != null) {
+                         datos += lectura;
+                     }
+                 } catch (IOException e) {
+                     datos = e.toString();
+                 }
+             }
 
              request.setAttribute("mensaje", datos);
              request.getRequestDispatcher("index.jsp").forward(request, response);
@@ -50,6 +86,12 @@ public class Servlet1 extends HttpServlet {
 
         return datos;
     }
+
+    @GetDatos(archivo = "C:\\Users\\ASUS\\IdeaProjects\\CES2\\datos.txt")
+    public void getDatos() {
+
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
